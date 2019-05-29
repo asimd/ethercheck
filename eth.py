5c78a1ab3d62cbf8fde9b22cd7af957c09de54e8
Asim Delalić <asim@mukuru.com>
2019-05-29 17:09:46 +0200

    fix example

--- eth.py
+++ eth.py
@@ -5,7 +5,7 @@ import time
 
 
 keys = set()
-with open('keys.txt') as f:
+with open('example.csv') as f:
 	for line in f.read().split('\n'):
 		if line:
 			repo, file, pkey = line.split(",")
@@ -15,7 +15,7 @@ for priv in keys:
 	try:
 		eth_addr = ethereum.utils.privtoaddr(priv)
 		eth_addr_hex = binascii.hexlify(eth_addr).decode("utf-8")
-		eth_balance = requests.get("https://api.etherscan.io/api?module=account&action=balance&address=0x" + eth_addr_hex + "&tag=latest" + "&apikey=JH9SMTEWZDBCVCWH47XR6K9RBNNRHSBGZC").json()["result"]
+		eth_balance = requests.get("https://api.etherscan.io/api?module=account&action=balance&address=0x" + eth_addr_hex + "&tag=latest" + "&apikey=YOURAPIKEYHERE").json()["result"]
 		print("ETH balance: {} address: 0x{} privkey: {}".format(float(eth_balance)/100000000, eth_addr_hex, priv))
 		time.sleep(3)
 	except (AssertionError, IndexError):
